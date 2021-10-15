2385c0f25b437fed6fb02239df5ee140f400ba79
VictorReis <rookie.qodeless@outlook.com>
2021-10-15 11:55:45 -0300

    jogo mortal de victor reis

---
+++ JogoMortal/Pisos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoMortal
+{
+    
+     public class Pisos
+    {
+        public string piso1;
+        public string piso2;
+        public string piso3;
+    }
+}
---
+++ JogoMortal/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace JogoMortal
+{
+    class Program:Pisos
+    {
+        static void Main(string[] args)
+        {
+            Pisos P = new Pisos();
+            Console.WriteLine("Escolha um lado?");
+            Console.WriteLine("ESQUERDA = 1");
+            Console.WriteLine("DIREITA = 2");
+            Console.WriteLine("1 ou 2?");
+            P.piso1 = Console.ReadLine();
+            if (P.piso1 == "1")
+            {
+                Console.WriteLine("Você Perdeu");
+            }
+            else
+            {
+                if(P.piso1 == "2")
+                {
+                    Console.WriteLine("Passou");
+                    Console.WriteLine("Escolha novamente?");
+                    P.piso2 = Console.ReadLine();
+                    if (P.piso2 == "2")
+                    {
+                        Console.WriteLine("Você Perdeu");
+                    }
+                    else
+                    {
+                        if (P.piso2 == "1")
+                        {
+                            Console.WriteLine("Passou");
+                            Console.WriteLine("Escolha novamente?");
+                            P.piso3 = Console.ReadLine();
+                            if (P.piso3 == "2")
+                            {
+                                Console.WriteLine("Você Perdeu");
+                            }
+                            else
+                            {
+                                if (P.piso3 == "1")
+                                {
+                                    Console.WriteLine("Parabens");
+                                    Console.WriteLine("Voce Ganhou");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
